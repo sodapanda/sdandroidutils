f386a21f8c98d5166b6adabe60ad25032cd0c63b
wangxiao <wangxiao1@u51.com>
2017-04-21 16:30:27 +0800

    [MDF]use take operator

--- sodautils/src/main/java/mobi/qishui/sodautils/phone/PhoneUtils.java
+++ sodautils/src/main/java/mobi/qishui/sodautils/phone/PhoneUtils.java
@@ -14,49 +14,13 @@ import com.trello.navi.NaviComponent;
 import com.trello.navi.rx.RxNavi;
 
 import mobi.qishui.sodautils.Constants;
-import mobi.qishui.sodautils.lifecycle.LifeCycleWatcher;
-import rx.Subscription;
 
 /**
  * Created by wangxiao on 2017/4/20.
  * Phone call utils
  */
 
-public class PhoneUtils implements LifeCycleWatcher {
-    private boolean mHasWatched;
-    private Activity mAct;
-    private String mPhoneNum;
-
-    public PhoneUtils(Activity mAct) {
-        this.mAct = mAct;
-    }
-
-    @Override
-    public Subscription watch(NaviComponent component) {
-        if (component == null) {
-            throw new IllegalArgumentException("component and events can't be null");
-        }
-
-        mHasWatched = true;
-
-        return RxNavi.observe(component, Event.REQUEST_PERMISSIONS_RESULT).subscribe((msg) -> {
-            if (msg.requestCode() != Constants.PERMISSION_CALL_CODE) {
-                return;
-            }
-
-            if (msg.grantResults().length > 0 &&
-                    msg.grantResults()[0] == PackageManager.PERMISSION_GRANTED) {
-                mAct.startActivity(getCallIntent());
-            }
-        });
-    }
-
-    private Intent getCallIntent() {
-        Intent intent = new Intent(Intent.ACTION_CALL);
-        intent.setData(Uri.parse("tel:" + mPhoneNum));
-        return intent;
-    }
-
+public class PhoneUtils {
     /**
      * open dialer with number
      *
@@ -73,30 +37,43 @@ public class PhoneUtils implements LifeCycleWatcher {
         context.startActivity(intent);
     }
 
+
+    private Intent getCallIntent(String number) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        return intent;
+    }
+
     /**
      * make a call not open a dialer
      *
      * @param context  context
      * @param phoneNum number to call
      */
-    public void directCall(Activity context, String phoneNum) {
+    public void directCall(Activity context, NaviComponent component, String phoneNum) {
         if (context == null || TextUtils.isEmpty(phoneNum)) {
             throw new IllegalArgumentException("context and number can't be null");
         }
-
-        if (!mHasWatched) {
-            throw new IllegalStateException("not watched!call watch first");
-        }
-
-        this.mPhoneNum = phoneNum;
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(context,
                     new String[]{Manifest.permission.CALL_PHONE},
                     Constants.PERMISSION_CALL_CODE);
+            RxNavi.observe(component, Event.REQUEST_PERMISSIONS_RESULT)
+                    .take(1)
+                    .subscribe((msg) -> {
+                        if (msg.requestCode() != Constants.PERMISSION_CALL_CODE) {
+                            return;
+                        }
+
+                        if (msg.grantResults().length > 0 &&
+                                msg.grantResults()[0] == PackageManager.PERMISSION_GRANTED) {
+                            context.startActivity(getCallIntent(phoneNum));
+                        }
+                    });
         } else {
-            context.startActivity(getCallIntent());
+            context.startActivity(getCallIntent(phoneNum));
         }
     }
 }
